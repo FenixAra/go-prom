1fddb9cae0c1100c0020ad67cf570392e2db4c38
Aravindhan <aravindhan.a@lynk.co.in>
2019-04-24 11:35:37 +0530

    ➕ Added name for tracking requests

--- prom/prom.go
+++ prom/prom.go
@@ -3,7 +3,6 @@ package prom
 import (
 	"net/http"
 	"runtime"
-	"strings"
 	"time"
 
 	"github.com/julienschmidt/httprouter"
@@ -86,28 +85,23 @@ func TrackDependency(dep, req, status string, v float64) {
 
 // Track function is a wrapper/closure over httprouter's handler. It will publish
 // the HTTP response time metrics to prometheus's /metrics
-func Track(h Handle) httprouter.Handle {
+func Track(h Handle, name string) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		st := time.Now()
 		status := h(w, r, ps)
 		method := r.Method
-		strs := strings.Split(r.RequestURI, "?")
-		req := ""
-		if len(strs) > 0 {
-			req = strs[0]
-		}
 
 		switch {
 		case status >= 500:
-			requestTime.WithLabelValues(Label5xx, req, method).Observe(float64(time.Since(st).Seconds()))
+			requestTime.WithLabelValues(Label5xx, name, method).Observe(float64(time.Since(st).Seconds()))
 		case status >= 400:
-			requestTime.WithLabelValues(Label4xx, req, method).Observe(float64(time.Since(st).Seconds()))
+			requestTime.WithLabelValues(Label4xx, name, method).Observe(float64(time.Since(st).Seconds()))
 		case status >= 300:
-			requestTime.WithLabelValues(Label3xx, req, method).Observe(float64(time.Since(st).Seconds()))
+			requestTime.WithLabelValues(Label3xx, name, method).Observe(float64(time.Since(st).Seconds()))
 		case status >= 200:
-			requestTime.WithLabelValues(Label2xx, req, method).Observe(float64(time.Since(st).Seconds()))
+			requestTime.WithLabelValues(Label2xx, name, method).Observe(float64(time.Since(st).Seconds()))
 		default:
-			requestTime.WithLabelValues(Label2xx, req, method).Observe(float64(time.Since(st).Seconds()))
+			requestTime.WithLabelValues(Label2xx, name, method).Observe(float64(time.Since(st).Seconds()))
 		}
 	}
 }
